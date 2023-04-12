ee7a6c15a622d46735d542e9f8a70aff8ecf8ca1
Ray Ma <rayma@outlook.com>
2023-04-12 20:38:04 +1000

    add new func

--- scripts.js
+++ scripts.js
@@ -8,3 +8,14 @@ function myFunc() {
         document.getElementById("myBtn").innerHTML = "Menu";
     }
 }
+
+function getMax() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+        document.getElementById("myBtn").innerHTML = "Close";
+    } else {
+        x.className = "topnav";
+        document.getElementById("myBtn").innerHTML = "Menu";
+    }
+}
